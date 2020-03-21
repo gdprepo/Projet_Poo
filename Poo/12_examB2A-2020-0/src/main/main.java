e9e32e02b9fec4250e89a0da54994223838eb731
dgabin <depairegabin@MacBook-Pro-de-dgabin.local>
2020-03-21 15:12:04 +0100

    UPD all main + acteur

---
+++ Poo/12_examB2A-2020-0/src/main/main.java
@@ -0,0 +1,14 @@
+package main;
+
+import com.b2.mysql.domain.Acteurs;
+
+public class main {
+
+	public static void main(String[] args){
+	System.out.println("Hello World");
+	Acteurs resA = new Acteurs(true);
+	
+	resA.toSystemout();
+
+}
+}
